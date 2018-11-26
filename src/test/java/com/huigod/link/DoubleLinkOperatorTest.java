9c7c7569cdf0afbfe7226b72049422773ad1c696
huigod <tenghuiit@163.com>
2018-11-26 20:33:39 +0800

    Double linked Test Unit

---
+++ src/test/java/com/huigod/link/DoubleLinkOperatorTest.java
@@ -0,0 +1,68 @@
+package com.huigod.link;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DoubleLinkOperatorTest {
+
+  private DoubleLinkOperator doubleLinkOperator = new DoubleLinkOperator();
+  private LinkDataStruct linkDataStruct = new LinkDataStruct();
+
+  @Before
+  public void init() {
+    Node node1 = new Node("A");
+    node1.setPrev(linkDataStruct.getHead());
+    linkDataStruct.getHead().setNext(node1);
+    Node node2 = new Node("B");
+    node1.setNext(node2);
+    node2.setPrev(node1);
+    Node node3 = new Node("C");
+    node2.setNext(node3);
+    node3.setPrev(node2);
+    Node node4 = new Node("D");
+    node3.setNext(node4);
+    node4.setPrev(node3);
+    node4.setNext(linkDataStruct.getTail());
+    linkDataStruct.getTail().setPrev(node4);
+    linkDataStruct.setLength(4);
+  }
+
+  @Test
+  public void testInsertNodeByIndex() {
+    Assert.assertTrue(doubleLinkOperator.insertNodeByIndex(linkDataStruct, 1, "AA"));
+    Assert.assertArrayEquals(new String[]{"AA", "A", "B", "C", "D"},
+        doubleLinkOperator.getAllElementHeadToTail(linkDataStruct));
+    Assert.assertTrue(doubleLinkOperator.insertNodeByIndex(linkDataStruct, 6, "E"));
+    Assert.assertArrayEquals(new String[]{"AA", "A", "B", "C", "D", "E"},
+        doubleLinkOperator.getAllElementHeadToTail(linkDataStruct));
+  }
+
+  @Test
+  public void testGetAllElementHeadToTail() {
+    Assert.assertArrayEquals(new String[]{"A", "B", "C", "D"},
+        doubleLinkOperator.getAllElementHeadToTail(linkDataStruct));
+  }
+
+  @Test
+  public void testGetAllElementTailToHead() {
+    Assert.assertArrayEquals(new String[]{"D", "C", "B", "A"},
+        doubleLinkOperator.getAllElementTailToHead(linkDataStruct));
+  }
+
+  @Test
+  public void testRemoveElementByIndex() {
+    Assert.assertFalse(doubleLinkOperator.removeElementByIndex(linkDataStruct, 0));
+    Assert.assertTrue(doubleLinkOperator.removeElementByIndex(linkDataStruct, 1));
+    Assert.assertArrayEquals(new String[]{"B", "C", "D"},
+        doubleLinkOperator.getAllElementHeadToTail(linkDataStruct));
+    Assert.assertFalse(doubleLinkOperator.removeElementByIndex(linkDataStruct, 4));
+    Assert.assertTrue(doubleLinkOperator.removeElementByIndex(linkDataStruct, 3));
+    Assert.assertArrayEquals(new String[]{"B", "C"},
+        doubleLinkOperator.getAllElementHeadToTail(linkDataStruct));
+    Assert.assertTrue(doubleLinkOperator.removeElementByIndex(linkDataStruct, 2));
+    Assert.assertTrue(doubleLinkOperator.removeElementByIndex(linkDataStruct, 1));
+    Assert.assertEquals(linkDataStruct.getHead(), linkDataStruct.getTail());
+  }
+
+}
