66bd6bc0c22fffeadeee356c0bbc3e89bde92789
huigod <tenghuiit@163.com>
2018-11-26 20:34:51 +0800

    Support Double linked data Structure

--- src/main/java/com/huigod/link/LinkDataStruct.java
+++ src/main/java/com/huigod/link/LinkDataStruct.java
@@ -5,10 +5,12 @@ public class LinkDataStruct {
   //头结点可以不用存储实际数据，可以用来存储辅助的数据
   //真正的数据从tail后面的节点开始
   private Node head;
+  private Node tail;
   private int length;
 
   LinkDataStruct() {
     head = new Node();
+    tail = head;
   }
 
   public Node getHead() {
@@ -25,5 +27,13 @@ public class LinkDataStruct {
 
   public void setLength(int length) {
     this.length = length;
+  }
+
+  public Node getTail() {
+    return tail;
+  }
+
+  public void setTail(Node tail) {
+    this.tail = tail;
   }
 }
--- src/main/java/com/huigod/link/Node.java
+++ src/main/java/com/huigod/link/Node.java
@@ -6,6 +6,8 @@ public class Node {
 
   private Node next;
 
+  private Node prev;
+
   Node() {
 
   }
@@ -13,6 +15,7 @@ public class Node {
   Node(String data) {
     this.data = data;
     this.next = null;
+    this.prev = null;
   }
 
   public String getData() {
@@ -29,5 +32,13 @@ public class Node {
 
   public void setNext(Node next) {
     this.next = next;
+  }
+
+  public Node getPrev() {
+    return prev;
+  }
+
+  public void setPrev(Node prev) {
+    this.prev = prev;
   }
 }
